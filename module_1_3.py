d8867b48a65cf1984bb873122c2a5ed5e6d7e61c
imrambro <bronislav.shuchkovskiy.51@gmail.com>
2024-12-11 05:14:58 +0300

    Add files via upload
    Исправленное задание

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,6 @@
 name = "Bro"
-print ("Name:" + name)
+print ("Name: " + name)
 age = 24
-print ("Age:"),print (age)
-new_age = 1
-print ("New age:"),print (age+new_age)
-is_student = age>new_age
-print("Is student:"), print (is_student)
+print ("Age:",age)
+age = age + 1
+print('Age:',age)
